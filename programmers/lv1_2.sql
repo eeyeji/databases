06d1bc6d9dd32898d13cba0b0071193d50122911
eeyeji <dldpwl026@gmail.com>
2022-08-31 17:54:55 +0900

    sql

---
+++ programmers/lv1_2.sql
@@ -0,0 +1,7 @@
+-- SELECT NAME, DATETIME
+-- ANIMAL_ID 역순 정렬하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59035
+
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC;
