db89f618591180829b90e61c92e4326a9485047e
eeyeji <dldpwl026@gmail.com>
2022-09-02 08:51:09 +0900

    PROGRAMMERS_GROUPBY

--- PROGRAMMERS_GROUPBY/lv2_10.sql
+++ PROGRAMMERS_GROUPBY/lv2_10.sql
@@ -1,7 +1,6 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/59408
--- 중복제거하기
--- SUM, MAX, MIN
-
-SELECT count(DISTINCT NAME) 
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59414
+-- String, Date
+-- DATETIME에서 DATE로 형변환
+SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, '%Y-%m-%d') AS '날짜' 
 FROM ANIMAL_INS
-WHERE NAME IS NOT NULL;
+ORDER BY ANIMAL_ID;
